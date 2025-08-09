75cd7b681493703b50c495b5da22c7ddbf34f057
azrlb <azrielalbian23@gmail.com>
2025-08-09 21:10:09 +0800

    fix: changing the 'Template' to 'template' in layout.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Template from "../components/Template";
+import Template from "../components/template";
 
 const inter = Inter({ subsets: ["latin"] });
 
